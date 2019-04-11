f962c7cb59fdb4414430fb931f20f2da0ffb690d
Matteo De Micheli <matteodem@protonmail.com>
2019-04-11 11:07:17 +0200

    Add simple 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,23 @@
+<template>
+  <layout>
+    <h1 class="pl2">
+      <span class="f-headline pt3">???</span>
+      <br />
+      <g-link to="/"><span class="gray" style="padding-left: 18px">Go back...</span></g-link>
+    </h1>
+  </layout>
+</template>
+
+<script>
+  export default {
+    metaInfo: {
+      title: '404',
+    },
+  }
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
